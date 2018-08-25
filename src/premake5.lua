2db231e74826cb3d71cd74e58e97cc9d26b6b9e3
JeppeSRC <jeppe97rc@gmail.com>
2018-08-25 06:27:30 +0200

    Added premake5 script

---
+++ src/premake5.lua
@@ -0,0 +1,95 @@
+workspace("TheHolyCompiler")
+    location "../solution/"
+    cppdialect "c++14"
+
+    configurations {
+        "Debug-Static",
+        "Debug-Shared",
+        "Release-Static",
+        "Release-Shared"
+    }
+
+    platforms "x64"
+
+    architecture "x64"
+
+    callingconvention "FastCall"
+
+    flags {
+        "MultiProcessorCompile",
+        "NoPCH"
+    }
+
+    filter("Debug-*")
+        defines {
+            "THC_DEBUG"
+        }
+
+        optimize "Off"
+        inlining "Disabled"
+    
+    filter("Release-*")
+        defines {
+            "THC_RELEASE"
+        }
+
+        flags {
+            "LinkTimeOptimization"
+        }
+
+        optimize "Speed"
+        inlining "Auto"
+
+    filter("*-Static")
+        defines {
+            "THC_BUILD_STATIC"
+        }
+    
+    filter("*-Shared")
+        defines {
+            "THC_BUILD_SHARED"
+        }
+
+    filter ""
+
+project("TheHolyCompiler-core")
+    location "../solution/TheHolyCompiler-core/"
+    targetdir "%{sln.location}/../bin/TheHolyCompiler-core/"
+    objdir "%{sln.location}/../bin/TheHolyCompiler-core/intermediates/"
+
+    filter("*-Static")
+        kind "StaticLib"
+    
+    filter("*-Shared")
+        kind "SharedLib"
+
+    filter("")
+
+
+    files {
+        "TheHolyCompiler-core/**.h",
+        "TheHolyCompiler-core/**.cpp",
+        "TheHolyCompiler-core/**.c"
+    }
+
+project("TheHolyCompiler")
+    removeconfigurations {
+        "Debug-Shared",
+        "Release-Shared"
+    }
+
+    kind "ConsoleApp"
+    location "../solution/TheHolyCompiler/"
+    targetdir "%{sln.location}/../bin/TheHolyCompiler/"
+    objdir "%{sln.location}/../bin/TheHolyCompiler/intermediates/"
+    filename "thc"
+
+    dependson "TheHolyCompiler-core"
+
+    files {
+        "TheHolyCompiler/**.h",
+        "TheHolyCompiler/**.cpp",
+        "TheHolyCompiler/**.c"
+    }
+
+
