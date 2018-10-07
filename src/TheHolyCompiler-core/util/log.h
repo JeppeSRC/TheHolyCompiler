38230d1ec4c1296b374c9756bda21cd1b846a16f
JeppeSRC <jeppe97rc@gmail.com>
2018-10-07 22:12:23 +0200

    Added assert macro

--- src/TheHolyCompiler-core/util/log.h
+++ src/TheHolyCompiler-core/util/log.h
@@ -27,6 +27,12 @@ SOFTWARE.
 #include <Windows.h>
 #include "string.h"
 
+#ifdef THC_DEBUG
+#define THC_ASSERT(x) if (!(x)) { printf("Assertion Failed (%s) -> File: \"%s\" Function: \"%s\" Line: %u\n", ##x, __FILE__, __FUNCSIG__, __LINE__); *((int*)0x0) = 0; }
+#else
+#define THC_ASSERT(x) 
+#endif
+
 namespace thc {
 namespace utils {
 
