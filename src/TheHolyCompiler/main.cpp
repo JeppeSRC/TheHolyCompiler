65a6104c4b8c90df725b02ab1c1db043321a4d19
JeppeSRC <jeppe97rc@gmail.com>
2018-11-25 13:49:04 +0100

    Added test in sandbox

---
+++ src/TheHolyCompiler/main.cpp
@@ -0,0 +1,35 @@
+#include <core/compiler/preprocessor/preprocessor.h>
+#include <core/compiler/parsing/line.h>
+#include <util/log.h>
+#include <Windows.h>
+
+using namespace thc;
+using namespace core;
+using namespace utils;
+using namespace parsing;
+using namespace preprocessor;
+
+
+
+int main() {
+	Log::SetOutputHandle(GetStdHandle(STD_OUTPUT_HANDLE));
+
+	char buf[1024];
+
+	GetCurrentDirectoryA(1024, buf);
+
+	String path(buf);
+
+	List<String> includes;
+	List<String> defines;
+
+	defines.Add("DADDY");
+
+	includes.Add(path);
+
+	String s = Line::ToString(PreProcessor::Run(path+"/test.thsl", defines, includes));
+
+	printf("%s\n", s.str);
+
+	return 0;
+}
