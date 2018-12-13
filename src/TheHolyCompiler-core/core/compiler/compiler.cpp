c0bf728f6641e35388ccaf9b81c2177f741aa657
JeppeSRC <jeppe97rc@gmail.com>
2018-12-13 22:21:34 +0100

    Parsing structs

--- src/TheHolyCompiler-core/core/compiler/compiler.cpp
+++ src/TheHolyCompiler-core/core/compiler/compiler.cpp
@@ -188,6 +188,9 @@ void Compiler::ParseTokens(List<Token>& tokens) {
 			ParseInOut(tokens, i, VariableScope::In);
 		} else if (token.type == TokenType::DataOut) {
 			ParseInOut(tokens, i, VariableScope::Out);
+		} else if (token.type == TokenType::DataStruct) {
+			CreateTypeStruct(tokens, i + 1);
+			tokens.RemoveAt(i);
 		}
 
 		i--;
