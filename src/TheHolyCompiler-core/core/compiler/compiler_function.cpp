b6794b3af5a969e9bdce42003fce380700e4f04f
JeppeSRC <theholyhorse.dev@gmail.com>
2020-01-06 15:51:36 +0100

    Moved some functions to their own files

---
+++ src/TheHolyCompiler-core/core/compiler/compiler_function.cpp
@@ -0,0 +1,382 @@
+/*
+MIT License
+
+Copyright (c) 2018 Jesper Hammarström
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "compiler.h"
+#include <util/utils.h>
+#include <core/preprocessor/preprocessor.h>
+#include <util/log.h>
+
+namespace thc {
+namespace core {
+namespace compiler {
+
+using namespace utils;
+using namespace parsing;
+using namespace type;
+using namespace instruction;
+
+auto  CmpFunc = [](const Token& curr, const TokenType& c) -> bool {
+	return curr.type == c;
+};
+
+void Compiler::ParseFunction(List<Token>& tokens, uint64 start) {
+	uint64 offset = 0;
+
+	const Token& returnType = tokens[start];
+
+	TypeBase* retType = CreateType(tokens, start, nullptr);
+
+	if (retType == nullptr) {
+		Log::CompilerError(returnType, "Unexpected symbol \"%s\" expected a valid return type");
+	}
+
+	const Token& name = tokens[start + offset++];
+
+	if (name.type != TokenType::Name) {
+		Log::CompilerError(name, "Unexpected symbol \"%s\" expected a valid name", name.string.str);
+	}
+
+	FunctionDeclaration* decl = new FunctionDeclaration;
+
+	decl->name = name.string;
+	decl->returnType = retType;
+
+	const Token& open = tokens[start + offset++];
+
+	if (open.type != TokenType::ParenthesisOpen) {
+		Log::CompilerError(open, "Unexpected symbol \"%s\" expected \"(\"", open.string.str);
+	}
+
+	const Token& close = tokens[start + offset];
+
+	if (close.type == TokenType::ParenthesisClose) {
+		offset++;
+		goto naughtyLabel;
+	}
+
+	while (true) {
+		Variable* param = new Variable;
+
+		param->scope = VariableScope::Function;
+
+		const Token& token = tokens[start + offset++];
+
+		param->isConstant = token.type == TokenType::ModifierConst;
+
+		if (!param->isConstant) {
+			offset--;
+		}
+
+		TypeBase* type = CreateType(tokens, start + offset, nullptr);
+
+		if (type == nullptr) {
+			const Token& tmp = tokens[start + offset];
+			Log::CompilerError(tmp, "Unexpected symbol \"%s\" expected valid type", tmp.string.str);
+		}
+
+		const Token& ref = tokens[start + offset++];
+
+		if (ref.type == TokenType::ModifierReference) {
+			param->type = CreateTypePointer(type, VariableScope::Function);
+		} else {
+			offset--;
+			param->type = type;
+		}
+
+		const Token& name = tokens[start + offset++];
+
+		if (name.type == TokenType::Name) {
+			param->name = name.string;
+			if (!CheckGlobalName(name.string)) {
+				Log::CompilerWarning(name, "Local parameter \"%s\" overriding global variable", name.string.str);
+			}
+		} else {
+			offset--;
+			param->name = "";
+		}
+
+		decl->parameters.Add(param);
+
+		const Token& delim = tokens[start + offset++];
+
+		if (delim.type == TokenType::ParenthesisClose) {
+			break;
+		} else if (delim.type == TokenType::Comma) {
+			continue;
+		} else {
+			Log::CompilerError(delim, "Unexpected symbol \"%s\" expected \")\" or \",\"", delim.string.str);
+		}
+	}
+
+naughtyLabel:
+
+	auto cmp = [](FunctionDeclaration* const& curr, FunctionDeclaration* const& other) {
+		return *curr == other;
+	};
+
+	const Token& bracket = tokens[start + offset++];
+
+	uint64 index = functionDeclarations.Find<FunctionDeclaration*>(decl, cmp);
+
+	if (index == ~0) {
+		CreateFunctionDeclaration(decl);
+	} else if (bracket.type == TokenType::SemiColon) {
+		Log::CompilerError(name, "Redeclaration of function \"%s\"", name.string.str);
+	}
+
+	if (bracket.type == TokenType::CurlyBracketOpen) {
+		if (index != ~0) {
+			FunctionDeclaration* old = decl;
+			decl = functionDeclarations[index];
+
+			for (uint64 i = 0; i < decl->parameters.GetCount(); i++) {
+				if ((decl->parameters[i]->name = old->parameters[i]->name) == "") {
+					Log::CompilerError(name, "Parameter %u needs a name!", i);
+				}
+			}
+		}
+
+		instructions.Add(decl->declInstructions);
+
+		if (decl->defined) {
+			Log::CompilerError(tokens[start], "Redefinition");
+		}
+
+		VariableStack localVariables(this, decl->parameters);
+
+		instructions.Add(new InstLabel);
+
+		uint64 index = instructions.GetCount();
+
+		ParseBody(decl, tokens, start + offset, &localVariables);
+
+		instructions.InsertList(index, localVariables.variableInstructions); //Add all OpVariable instructions at the beginning of the first block
+
+		instructions.Add(new InstFunctionEnd);
+
+		decl->defined = true;
+	} else if (bracket.type != TokenType::SemiColon) {
+		Log::CompilerError(bracket, "Unexpected symbol \"%s\" expected \";\" or \"{\"", bracket.string.str);
+	}
+
+	tokens.Remove(start, start + offset - 1);
+}
+
+
+Compiler::ResultVariable Compiler::ParseFunctionCall(List<Token>& tokens, ParseInfo* info, VariableStack* localVariables) {
+	Token functionName = tokens[info->start];
+
+	ParseInfo inf;
+	inf.start = info->start + 1;
+
+	List<ResultVariable> parameterResults = ParseParameters(tokens, &inf, localVariables);
+
+	info->len = inf.len;
+
+	ResultVariable r;
+
+	uint64 fOffset = 0;
+
+	List<FunctionDeclaration*> decls = GetFunctionDeclarations(functionName.string);
+
+	if (!decls.GetCount()) {
+		Log::CompilerError(functionName, "No function with name \"%s\"", functionName.string.str);
+	}
+
+
+	for (uint64 i = 0; i < decls.GetCount(); i++) {
+		if (decls[i]->parameters.GetCount() != parameterResults.GetCount()) {
+			decls.RemoveAt(i--);
+		}
+	}
+
+	if (decls.GetCount() == 0) {
+		Log::CompilerError(functionName, "No overloaded version of function \"%s()\" takes %llu arguments", functionName.string.str, parameterResults.GetCount());
+	}
+
+	for (uint64 i = 0; i < parameterResults.GetCount(); i++) {
+		ResultVariable& res = parameterResults[i];
+
+		for (uint64 j = 0; j < decls.GetCount(); j++) {
+			FunctionDeclaration* d = decls[j];
+
+			Variable* param = d->parameters[i];
+			TypeBase* dt = param->type;
+
+			if (dt->type == Type::Pointer) {
+				//pass by reference but argument is rvalue
+				if (!res.isVariable /*&& !param->isConstant*/) {
+					if (decls.GetCount() == 1) {
+						Log::CompilerError(functionName, "argument %llu in \"%s\" must be a lvalue", i, functionName.string.str);
+					} else {
+						decls.RemoveAt(j--);
+						continue;
+					}
+				}
+			} else if (*dt != res.type) {
+				if (decls.GetCount() == 1) {
+					ID* operandId = res.id;
+
+					if (res.isVariable) {
+						InstLoad* load = new InstLoad(res.type->typeId, res.id, 0);
+						instructions.Add(load);
+
+						operandId = load->id;
+					}
+
+					TypeBase* tmp = res.type;
+					res = ImplicitCast(dt, res.type, operandId, &functionName);
+
+				} else {
+					decls.RemoveAt(j--);
+					continue;
+				}
+			}
+		}
+	}
+
+	FunctionDeclaration* decl = decls[0];
+
+	List<ID*> ids = Compiler::GetIDs(parameterResults);
+
+	InstFunctionCall* call = new InstFunctionCall(decl->returnType->typeId, decl->id, (uint32)ids.GetCount(), ids.GetData());
+	instructions.Add(call);
+
+	r.id = call->id;
+	r.type = decl->returnType;
+	r.isVariable = false;
+
+	return r;
+}
+
+Compiler::ResultVariable Compiler::ParseTypeConstructor(List<Token>& tokens, ParseInfo* info, VariableStack* localVariables) {
+	info->end = 0;
+
+	Token tmp = tokens[info->start];
+
+	TypePrimitive* type = (TypePrimitive*)CreateType(tokens, info->start, &info->end);
+
+	List<ResultVariable> parameters = ParseParameters(tokens, info, localVariables);
+
+	ResultVariable res;
+	res.type = type;
+	res.isConstant = false;
+	res.isVariable = false;
+
+	InstBase* inst = nullptr;
+
+	if (type->type == Type::Vector) {
+		uint8 numComponents = 0;
+
+		for (uint64 i = 0; i < parameters.GetCount(); i++) {
+			TypePrimitive* t = (TypePrimitive*)parameters[i].type;
+
+			if (type->componentType != t->componentType || type->bits != t->bits) {
+				Log::CompilerError(tmp, "Argument \"%s\"(%llu) is not compatible with \"%s\"", t->typeString.str, i, type->typeString.str);
+			}
+
+			numComponents += t->rows > 0 ? t->rows : 1;
+		}
+
+		if (numComponents != type->rows) {
+			Log::CompilerError(tmp, "Total component count must be %llu is %u", type->rows, numComponents);
+		}
+
+		List<ID*> ids = Compiler::GetIDs(parameters);
+
+		inst = new InstCompositeConstruct(type->typeId, (uint32)ids.GetCount(), ids.GetData());
+	} else if (type->type == Type::Matrix) {
+		if (parameters.GetCount() != type->columns) {
+			Log::CompilerError(tmp, "Argument count must be %llu is %u", parameters.GetCount(), type->columns);
+		}
+
+		for (uint64 i = 0; i < parameters.GetCount(); i++) {
+			TypePrimitive* t = (TypePrimitive*)parameters[i].type;
+
+			if (t->componentType != type->componentType || t->bits != type->bits || t->rows != type->rows || t->type != Type::Vector) {
+				Log::CompilerError(tmp, "Argument \"%s\"(%llu) is not compatible with \"%s\"", t->typeString.str, i, type->typeString.str);
+			}
+		}
+
+		List<ID*> ids = Compiler::GetIDs(parameters);
+
+		inst = new InstCompositeConstruct(type->typeId, (uint32)ids.GetCount(), ids.GetData());
+	} else {
+		Log::CompilerError(tmp, "\"%s\" doesn't have a constructor", type->typeString.str);
+	}
+
+	instructions.Add(inst);
+	res.id = inst->id;
+
+	return res;
+}
+
+List<Compiler::ResultVariable> Compiler::ParseParameters(List<Token>& tokens, ParseInfo* info, VariableStack* localVariables) {
+	uint64 offset = info->start;
+
+	const Token& parenthesisOpen = tokens[offset];
+
+	if (parenthesisOpen.type != TokenType::ParenthesisOpen) {
+		Log::CompilerError(parenthesisOpen, "Unexpected symbol \"%s\" expected \"(\"", parenthesisOpen.string.str);
+	}
+
+	uint64 parenthesisClose = FindMatchingToken(tokens, offset++, TokenType::ParenthesisOpen, TokenType::ParenthesisClose);
+
+	if (parenthesisClose == ~0) {
+		Log::CompilerError(parenthesisOpen, "\"(\" needs a closing \")\"");
+	}
+
+
+	List<ResultVariable> parameterResults;
+
+	bool moreParams = true;
+
+	do {
+		uint64 end = tokens.Find<TokenType>(TokenType::Comma, CmpFunc, offset);
+
+		if (end-- > parenthesisClose) {
+			end = parenthesisClose - 1;
+			moreParams = false;
+		}
+
+		ParseInfo inf;
+
+		inf.start = offset;
+		inf.end = end;
+
+		ResultVariable res = ParseExpression(tokens, &inf, localVariables);
+
+		offset = inf.end + 2;
+
+		parameterResults.Add(res);
+	} while (moreParams);
+
+	info->len = offset - info->start;
+
+	return std::move(parameterResults);
+}
+
+}
+}
+}
---
+++ src/TheHolyCompiler-core/core/compiler/compiler_pipeline.cpp
@@ -0,0 +1,241 @@
+/*
+MIT License
+
+Copyright (c) 2018 Jesper Hammarström
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+#include "compiler.h"
+#include <util/utils.h>
+#include <core/preprocessor/preprocessor.h>
+#include <util/log.h>
+
+namespace thc {
+namespace core {
+namespace compiler {
+
+using namespace utils;
+using namespace parsing;
+using namespace type;
+using namespace instruction;
+
+void Compiler::ParseLayout(List<Token>& tokens, uint64 start) {
+	uint64 offset = 0;
+
+	const Token& parenthesisOpen = tokens[++start + offset++];
+
+	if (parenthesisOpen.type != TokenType::ParenthesisOpen) {
+		Log::CompilerError(parenthesisOpen, "Unexpected symbol \"%s\" expected \"(\"", parenthesisOpen.string.str);
+	}
+
+	uint32 location = ~0;
+	uint32 binding = ~0;
+	uint32 set = ~0;
+
+	auto GetValue = [&tokens, &offset, start]() -> uint32 {
+		const Token& equal = tokens[start + offset++];
+
+		if (equal.type != TokenType::OperatorAssign) {
+			Log::CompilerError(equal, "Unexpected symbol \"%s\" expected \"=\"", equal.string.str);
+		}
+
+		const Token& value = tokens[start + offset++];
+
+		if (value.type != TokenType::Value) {
+			Log::CompilerError(value, "Unexpected symbol \"%s\" expected a valid value", value.string.str);
+		}
+
+		return (uint32)value.value;
+	};
+
+	while (true) {
+		const Token& specifier = tokens[start + offset++];
+
+		if (specifier.type != TokenType::Name && !(specifier.string == "location" || specifier.string == "set" || specifier.string == "binding")) {
+			Log::CompilerError(specifier, "Unexpected symbol \"%s\" expected \"location, set or binding\"", specifier.string.str);
+		}
+
+		if (specifier.string == "location") {
+			if (location != ~0) {
+				Log::CompilerError(specifier, "Specifier \"location\" already specified once");
+			}
+
+			location = GetValue();
+		} else if (specifier.string == "binding") {
+			if (binding != ~0) {
+				Log::CompilerError(specifier, "Specifier \"binding\" already specified once");
+			}
+
+			binding = GetValue();
+		} else if (specifier.string == "set") {
+			if (set != ~0) {
+				Log::CompilerError(specifier, "Specifier \"set\" already specified once");
+			}
+
+			set = GetValue();
+		}
+
+		const Token& next = tokens[start + offset++];
+
+		if (next.type == TokenType::Comma) {
+			continue;
+		} else if (next.type == TokenType::ParenthesisClose) {
+			break;
+		} else {
+			Log::CompilerError(next, "Unexpected symbol \"%s\" expected \")\"", next.string.str);
+		}
+	}
+
+	const Token& scope = tokens[start + offset++];
+
+	Variable tmp;
+
+	switch (scope.type) {
+		case TokenType::DataIn:
+			tmp.scope = VariableScope::In;
+
+			if (binding != ~0) {
+				Log::CompilerError(scope, "Specifier \"binding\" cannot be used on \"in\"");
+			} else if (set != ~0) {
+				Log::CompilerError(scope, "Specifier \"set\" cannot be used on \"in\"");
+			} else if (location == ~0) {
+				Log::CompilerError(scope, "Specifier \"location\" must be set");
+			}
+
+			break;
+		case TokenType::DataOut:
+			tmp.scope = VariableScope::Out;
+
+			if (binding != ~0) {
+				Log::CompilerError(scope, "Specifier \"binding\" cannot be used on \"out\"");
+			} else if (set != ~0) {
+				Log::CompilerError(scope, "Specifier \"set\" cannot be used on \"out\"");
+			} else if (location == ~0) {
+				Log::CompilerError(scope, "Specifier \"location\" must be set");
+			}
+
+			break;
+		case TokenType::DataUniform:
+			tmp.scope = VariableScope::Uniform;
+
+			if (binding == ~0) {
+				Log::CompilerError(scope, "Specifier \"binding\" must be set");
+			} else if (set == ~0) {
+				Log::CompilerError(scope, "Specifier \"set\" must be set");
+			} else if (location != ~0) {
+				Log::CompilerError(scope, "Specifier \"location\" cannot be used on \"uniform\"");
+			}
+
+			break;
+		default:
+			Log::CompilerError(scope, "Unexpected symbol \"%s\" expected \"in, out or uniform\"", scope.string.str);
+	}
+
+	Variable* var;
+
+	if (tmp.scope == VariableScope::Uniform) {
+		TypeStruct* str = CreateTypeStruct(tokens, start + offset++, nullptr);
+
+		String name = str->typeString;
+		str->typeString += "_uniform_type";
+
+		var = CreateGlobalVariable(str, tmp.scope, name);
+
+		annotationIstructions.Add(new InstDecorate(var->variableId, THC_SPIRV_DECORATION_BINDING, &binding, 1));
+		annotationIstructions.Add(new InstDecorate(var->variableId, THC_SPIRV_DECORATION_DESCRIPTORSET, &set, 1));
+	} else {
+		TypePrimitive* type = CreateTypePrimitive(tokens, start + offset, nullptr);
+
+		const Token& name = tokens[start + offset++];
+
+		if (name.type != TokenType::Name) {
+			Log::CompilerError(name, "Unexpected symbol \"%s\" expected a valid name", name.string.str);
+		}
+
+		if (!CheckGlobalName(name.string)) {
+			Log::CompilerError(name, "Redefinition of global variable \"%s\"", name.string.str);
+		}
+
+		const Token& semiColon = tokens[start + offset++];
+
+		if (semiColon.type != TokenType::SemiColon) {
+			Log::CompilerError(semiColon, "Unexpected symbol \"%s\" expected \";\"", semiColon.string.str);
+		}
+
+		var = CreateGlobalVariable(type, tmp.scope, name.string);
+
+		annotationIstructions.Add(new InstDecorate(var->variableId, THC_SPIRV_DECORATION_LOCATION, &location, 1));
+	}
+
+	start--;
+
+	tokens.Remove(start, start + offset - 1);
+}
+
+void Compiler::ParseInOut(List<Token>& tokens, uint64 start, VariableScope scope) {
+	uint64 offset = 0;
+
+	TypePrimitive* type = CreateTypePrimitive(tokens, ++start + offset, nullptr);
+
+	const Token& name = tokens[start + offset++];
+
+	if (name.type != TokenType::Name) {
+		Log::CompilerError(name, "Unexpected symbol \"%s\" expected a valid name", name.string.str);
+	}
+
+	const Token& assign = tokens[start + offset++];
+
+	if (assign.type != TokenType::OperatorAssign) {
+		Log::CompilerError(assign, "Unexpected symbol \"%s\" expected \"=\"", assign.string.str);
+	}
+
+	const Token& intrin = tokens[start + offset++];
+
+	if (intrin.type != TokenType::Name) {
+		Log::CompilerError(intrin, "Unexpected symbol \"%s\" expected a valid name", intrin.string.str);
+	}
+
+	const Token& semi = tokens[start + offset++];
+
+	if (semi.type != TokenType::SemiColon) {
+		Log::CompilerError(semi, "Unexpected symbol \"%s\" expected \";\"", semi.string.str);
+	}
+
+	Variable* var = CreateGlobalVariable(type, scope, name.string);
+
+	if (intrin.string == "THSL_Position") {
+		uint32 builtin = 0;
+
+		if (var->scope != VariableScope::Out) {
+			Log::CompilerError(intrin, "Builtin THSL_Position must be an output variable");
+		}
+
+		annotationIstructions.Add(new InstDecorate(var->variableId, THC_SPIRV_DECORATION_BUILTIN, &builtin, 1));
+	}
+
+	start--;
+
+	tokens.Remove(start, start + offset);
+}
+
+}
+}
+}
